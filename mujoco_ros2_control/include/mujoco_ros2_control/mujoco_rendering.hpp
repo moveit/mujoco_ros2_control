3c8a5b003200714fedf1d2b52218da43aecda104
Erik Holum <eholum@gmail.com>
2025-02-18 09:37:45 -0500

    Add basic camera support to the mujoco rendering module

--- mujoco_ros2_control/include/mujoco_ros2_control/mujoco_rendering.hpp
+++ mujoco_ros2_control/include/mujoco_ros2_control/mujoco_rendering.hpp
@@ -21,12 +21,38 @@
 #ifndef MUJOCO_ROS2_CONTROL__MUJOCO_RENDERING_HPP_
 #define MUJOCO_ROS2_CONTROL__MUJOCO_RENDERING_HPP_
 
+#include <string>
+#include <vector>
+
 #include "GLFW/glfw3.h"
 #include "mujoco/mujoco.h"
 #include "rclcpp/rclcpp.hpp"
 
+#include "rclcpp/node.hpp"
+#include "rclcpp/publisher.hpp"
+#include "sensor_msgs/msg/camera_info.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
 namespace mujoco_ros2_control
 {
+
+struct CameraData
+{
+  mjvCamera mjv_cam;
+  mjrRect viewport;
+
+  std::string name;
+  std::string frame_name;
+  uint32_t width;
+  uint32_t height;
+
+  sensor_msgs::msg::Image image;
+  sensor_msgs::msg::CameraInfo camera_info;
+
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub;
+  rclcpp::Publisher<sensor_msgs::msg::CameraInfo>::SharedPtr camera_info_pub;
+};
+
 class MujocoRendering
 {
 public:
@@ -37,29 +63,41 @@ public:
   void init(rclcpp::Node::SharedPtr &node, mjModel *mujoco_model, mjData *mujoco_data);
   bool is_close_flag_raised();
   void update();
+  void update_cameras();
   void close();
 
 private:
+
   MujocoRendering();
   static void keyboard_callback(GLFWwindow *window, int key, int scancode, int act, int mods);
   static void mouse_button_callback(GLFWwindow *window, int button, int act, int mods);
   static void mouse_move_callback(GLFWwindow *window, double xpos, double ypos);
   static void scroll_callback(GLFWwindow *window, double xoffset, double yoffset);
+
   void keyboard_callback_impl(GLFWwindow *window, int key, int scancode, int act, int mods);
   void mouse_button_callback_impl(GLFWwindow *window, int button, int act, int mods);
   void mouse_move_callback_impl(GLFWwindow *window, double xpos, double ypos);
   void scroll_callback_impl(GLFWwindow *window, double xoffset, double yoffset);
 
-  static MujocoRendering *instance_;
-  rclcpp::Node::SharedPtr node_;  // TODO(sangtaek): delete node and add logger
-  mjModel *mj_model_;
-  mjData *mj_data_;
+  void register_cameras();
+
+  static MujocoRendering* instance_;
+  rclcpp::Node::SharedPtr node_;
+
+  mjModel* mj_model_;
+  mjData* mj_data_;
+
+  // Window and primary camera for the simulation's viewer
+  GLFWwindow* window_;
   mjvCamera mjv_cam_;
+
+  // Options for the rendering context and scene, all of these are hard coded to defaults.
   mjvOption mjv_opt_;
   mjvScene mjv_scn_;
   mjrContext mjr_con_;
 
-  GLFWwindow *window_;
+  // Additional user specified cameras
+  std::vector<CameraData> cameras_;
 
   bool button_left_;
   bool button_middle_;
