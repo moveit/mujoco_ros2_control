79342f414c3716afd6876509a6e77b638e9d523e
Erik Holum <eholum@gmail.com>
2025-03-03 11:08:42 -0500

    Add joint to make camera oscillate in the demo

--- mujoco_ros2_control_demos/launch/camera_example.launch.py
+++ mujoco_ros2_control_demos/launch/camera_example.launch.py
@@ -20,6 +20,8 @@ def generate_launch_description():
                               'urdf',
                               'test_camera.xacro.urdf')
 
+    controller_config_file = os.path.join(mujoco_ros2_control_demos_path, 'config', 'camera_controller_position.yaml')
+
     doc = xacro.parse(open(xacro_file))
     xacro.process_doc(doc)
     robot_description = {'robot_description': doc.toxml()}
@@ -30,10 +32,43 @@ def generate_launch_description():
         output='screen',
         parameters=[
             robot_description,
+            controller_config_file,
             {'mujoco_model_path':os.path.join(mujoco_ros2_control_demos_path, 'mujoco_models', 'test_camera.xml')}
         ]
     )
 
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_position_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'position_controller'],
+        output='screen'
+    )
+
     return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessStart(
+                target_action=node_mujoco_ros2_control,
+                on_start=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_position_controller],
+            )
+        ),
         node_mujoco_ros2_control,
+        node_robot_state_publisher,
     ])
